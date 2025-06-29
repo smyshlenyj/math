180e53cfb545a6a96bbeb45a248d1781f50dedf0
j <smyshlenyj@gmail.com>
2025-06-29 22:41:02 +0300

    - iutput default constructor

--- include/math/math.h
+++ include/math/math.h
@@ -3,42 +3,37 @@
 
 namespace math {
 struct Input {
-  int first;
-  int second;
-  char action;
+    int first;
+    int second;
+    char action;
 
-
-  Input() = default;
-  Input(int _first, int _second, char _action){
-    first = _first;
-    second = _second;
-    action = _action;
-  }
+    Input() = default;
+    Input(int _first, int _second, char _action) {
+        first = _first;
+        second = _second;
+        action = _action;
+    }
 };
 
 template <typename T>
 struct Output {
-  
-  T result;
-  std::string status;
+    T result;
+    std::string status;
 
     Output() = default;
     Output(T _result, std::string _status) {
-    result = _result;
-    status = _status;
-  }
+        result = _result;
+        status = _status;
+    }
 };
 
 class Math {
-
-public:
-  Math();
-
-  Output<int> add(const Input& input);
-  Output<int> subtract(const Input& input);
-  Output<int> multiply(const Input& input);
-  Output<double> divide(const Input& input);
-  Output<int> pow(const Input& input);
-  Output<int> factorial(const Input& input);
+   public:
+    Output<int> add(const Input& input);
+    Output<int> subtract(const Input& input);
+    Output<int> multiply(const Input& input);
+    Output<double> divide(const Input& input);
+    Output<int> pow(const Input& input);
+    Output<int> factorial(const Input& input);
 };
-} // namespace math
+}  // namespace math
